826d41d5a14bf099ee17bd45d26870e7298db2d6
Sourabh Tripathi <sourabhtripathi@outlook.com>
2013-11-17 22:53:02 +0530

    v0.2 printing subsequence

--- lds.c
+++ lds.c
@@ -3,12 +3,13 @@
 main()
 {
   int data[]={15,2,10,16,18,8,6,9,2,14};
-  //int data[]={1,2,3,4,5,6,7,8,9,10};
-  int temp[10]={0};
-  int point[10];
+  // int data[]={1,2,3,4,5,6,7,8,9,10};
+  int temp[LEN]={0};
+  int point[LEN],sub[LEN];
   int i,j,max=-1,total=0;
   temp[0]=1;
-  for(i=1;i<10;i++)
+  point[0]=-1;
+  for(i=1;i<LEN;i++)
     {
       max=0;
       temp[i]=0;
@@ -27,11 +28,26 @@ main()
 	  total=temp[i];
 	}
     }
-  for(i=0;i<10;i++)
+  /*for(i=0;i<LEN;i++)
     printf("%d ",temp[i]);
   printf("\n");
-  for(i=0;i<10;i++)
+  for(i=0;i<LEN;i++)
     printf("%d ",point[i]);
-  printf("\n");
-  printf("%d \n",total);
+    printf("\n");*/
+  printf("length of the longest decreasing subsequence is %d \n",total);
+  j=total;
+  for(i=LEN;i>=0;)
+    {
+      if(temp[i]==total)
+	{
+	  sub[total]=data[i];
+	  i=point[i];
+	  total--;
+	}
+      else
+	i--;
+    }
+  for(i=1;i<=j;i++)
+    printf("%d ",sub[i]);
+  printf("\nprinted the longest decreasing subsequence \n");
 }
